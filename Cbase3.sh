8f94aea69c39fd4f93aaaa0b7793b15a35d897c0
Jaime <74079489+user-jaime@users.noreply.github.com>
2022-07-22 15:09:17 -0700

    Add files via upload

---
+++ Cbase3.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/bash 
+# Cbase3.sh (DEBIAN EDITION) 01June2022
+
+cp -r /media/kodi/jaime2/POSTOS/Flatery-Dark.tar.gz ~
+cp -r /media/kodi/jaime2/POSTOS/05-Flat-Remix-GTK-Blue-Dark_20201129.tar.xz ~
+cp -r /media/kodi/jaime2/POSTOS/Dracula-alt-style.tar.xz ~
+cp -r /media/kodi/jaime2/POSTOS/sparrow_*_amd64.deb ~
+cp -r /media/kodi/jaime2/POSTOS/veracrypt-1.25.9-Debian-11-amd64.deb ~
+cp -r /media/kodi/jaime2/Wallpaper ~
+cp -r /media/kodi/jaime2/POSTOS/PM.*.odt ~
+cp -r /media/kodi/jaime2/POSTOS/Fluent.Reader.*.AppImage ~
+
+mkdir ~/.icons ~/.themes ~/Scripts
+rm -r ~/Music ~/Public ~/Templates ~/Videos
+
+tar -xvf Flatery-Dark.tar.gz
+mv Flatery-Dark ~/.icons
+sleep 3
+rm -r Flatery-Dark.tar.gz
+
+tar -xvJf 05-Flat-Remix-GTK-Blue-Dark_20201129.tar.xz
+mv Flat-Remix-GTK-Blue-Dark ~/.themes
+sleep 3
+rm -r 05-Flat-Remix-GTK-Blue-Dark_20201129.tar.xz
+
+tar -xvJf Dracula-alt-style.tar.xz
+mv Dracula-alt-style ~/.themes
+sleep 3
+rm -r Dracula-alt-style.tar.xz
+
+sudo dpkg -i sparrow_*_amd64.deb
+sleep 3
+rm -r sparrow_*_amd64.deb
+
+sudo dpkg -i veracrypt-1.25.9-Debian-11-amd64.deb
+sleep 3
+rm -r veracrypt-1.25.9-Debian-11-amd64.deb
+apt --fix-broken install
+
+usb="/media/kodi/jaime2/POSTOS/Ddot"
+dl="rsync -avh --delete"
+
+$dl $usb/.cache/ ~/.cache
+$dl $usb/.config/ ~/.config
+$dl $usb/.local/ ~/.local
+$dl $usb/.mozilla/ ~/.mozilla
+$dl $usb/.sparrow/ ~/.sparrow
+$dl $usb/.vim/ ~/.vim
+$dl $usb/.bash_aliases ~/.bash_aliases
+$dl $usb/.bashrc ~/.bashrc
+$dl $usb/.profile ~/.profile
+$dl $usb/.tmux.conf ~/.tmux.conf
+$dl $usb/.viminfo ~/.viminfo
+$dl $usb/.vimrc ~/.vimrc
+$dl $usb/.Xresources ~/.Xresources
+
+cd ~
+rm *.sh
+cd /media/kodi/jaime2/POSTOS/DMaint
+cp -r bye.sh daily.sh rsync.sh sd.sh wallsync.sh ~
+cd ~
+chmod 700 *.sh
+sleep 5
+systemctl reboot
---
+++ install.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/bash
+# Cbase0.sh 12July2022
+
+# *** SCRIPT MUST BE RUN AS ROOT ***
+
+if [ "$EUID" -ne 0 ]
+  then echo ""
+  echo "           *** YA GOTTA BE ROOT TO RUN THIS SCRIPT!! *** "
+  echo ""
+  exit
+fi
+
+clear 
+set -e
+
+cd /etc/apt
+echo '' >> sources.list
+echo 'deb http://deb.debian.org/debian unstable main contrib non-free' >> sources.list
+echo 'deb-src http://deb.debian.org/debian unstable main contrib non-free' >> sources.list
+echo '' >> sources.list
+cat sources.list
+sleep 5
+
+cd /etc/apt
+echo 'Package: *' >> preferences
+echo 'Pin: release a=bullseye' >> preferences
+echo 'Pin-Priority: 500' >> preferences
+echo '' >> preferences
+echo 'Package: *' >> preferences
+echo 'Pin: release a=unstable' >> preferences
+echo 'Pin-Priority: 100' >> preferences
+echo '' >> preferences
+echo 'Package: gnome-tweaks' >> preferences
+echo 'Pin: release a=unstable' >> preferences
+echo 'Pin-Priority: 1000' >> preferences
+echo '' >> preferences
+echo 'Package: gnome-session' >> preferences
+echo 'Pin: release a=unstable' >> preferences
+echo 'Pin-Priority: 1000' >> preferences
+echo '' >> preferences
+echo 'Package: gnome-backgrounds' >> preferences
+echo 'Pin: release a=unstable' >> preferences
+echo 'Pin-Priority: 1000' >> preferences
+echo '' >> preferences
+echo 'Package: gnome-applets' >> preferences
+echo 'Pin: release a=unstable' >> preferences
+echo 'Pin-Priority: 1000' >> preferences
+echo '' >> preferences
+echo 'Package: gnome-control-center' >> preferences
+echo 'Pin: release a=unstable' >> preferences
+echo 'Pin-Priority: 1000' >> preferences
+echo '' >> preferences
+echo 'Package: mutter' >> preferences
+echo 'Pin: release a=unstable' >> preferences
+echo 'Pin-Priority: 1000' >> preferences
+echo '' >> preferences
+echo 'Package: gjs' >> preferences
+echo 'Pin: release a=unstable' >> preferences
+echo 'Pin-Priority: 1000' >> preferences
+
+cd
+
+apt update;apt upgrade
+
+apt -t unstable install gnome-session gnome-shell gnome-backgrounds gnome-applets gnome-control-center mutter gjs
+
+apt update;apt upgrade
+
+insa="apt -y install"
+
+read -r -p "Install Chromium? [Y/n] " prompt
+if [ "$prompt" == "Y" ]
+then
+$insa chromium
+fi
+
+$insa curl
+$insa firefox-esr
+$insa ffmpeg
+$insa git
+$insa gnome-shell-extensions
+$insa libreoffice-writer
+$insa neofetch
+$insa okular
+$insa resolvconf
+$insa rsync
+$insa shellcheck
+$insa stacer
+$insa sudo
+$insa terminator
+$insa tmux
+$insa ufw
+$insa vim
+$insa vlc
+$insa wget
+$insa xterm
+
+apt update;apt upgrade
+
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+sleep 5
+cd
+echo '' >> .bashrc
+echo '### STARSHIP PROMPT ###' >> .bashrc
+echo 'eval "$(starship init bash)"' >> .bashrc
+cat .bashrc
+sleep 2
+apt -y remove kdeconnect
+apt -y autoremove
+
+apt update;apt upgrade
+systemctl reboot
+
+#source ~/POSTOS-D/Cbase3.sh
